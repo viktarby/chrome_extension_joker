0d6f7275685d9f51e19c5038c790382fc1f1cfdd
= <=>
2018-06-28 18:33:38 +0300

    Add styles and event handlers to extension html

--- popup.js
+++ popup.js
@@ -8,13 +8,14 @@ customValue.addEventListener("change", function(e){
     e.preventDefault();
     e.stopPropagation();
     hero.style.display = "none";
+    msg.style.display = "none";
     if (e.currentTarget.value === '3') {
         hero.style.display = "block";
     }
 });
 
 save.addEventListener("click", function(){
-    msg.innerText = "Saved";
+    msg.style.display = "block";
     var creature = customValue.selectedOptions[0].value =="3" ? hero.value :  customValue.selectedOptions[0].text;
     var searchUrl = "http://www.google.fr/search?tbm=isch&tbs=isz:ex,iszw:800,iszh:600&q=" + creature;
     frameDom.src = searchUrl;
